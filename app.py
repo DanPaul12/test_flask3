7a3b794975691abfcfa9adf43ed3a0e0085ac061
DanPaul12 <dan.paul.schechter@gmail.com>
2024-12-11 17:24:58 -0800

    making postgresql db cconnection

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://example_sum_postgres_3qkn_user:nSkwUR0ygS2dEEWA09qgGV9jBApRe8To@dpg-ctd3aijqf0us73bk5080-a.oregon-postgres.render.com/example_sum_postgres_3qkn'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 def add_numbers(a, b):
     return a + b
